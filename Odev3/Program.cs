1073a386621b8d216a73ca73b7da32564ffe9dd7
jackup <yk.58@hotmail.com>
2021-11-23 20:45:41 +0300

    Add project files.

---
+++ Odev3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu uygulama örneğimizden toplam 4 adet metot kullanmaktayız.
+            //Bu kullandığımız metotların adları ise sırası ile; yiyecek, içecek, tatlılar ve hesap işlemlerini yerine getiren metotlardır.
+            //Uygulamamızı kullanan kullanıcılar uygulamayı açtıklarında karşılarına menü seçenekleri çıkıyor.
+            //Bu seçeneklerin seçimine ve toplam hesap bilgilerine göre metotlarımız devreye girip hesaplıyor. 
+            //Sonucu da ekrana yazdırıyor.
+
+
+        }
+    }
+}
